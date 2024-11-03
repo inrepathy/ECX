cfab10f7a49c6ba21c65081c7b16052c5477ae12
inrepathy <137946035+inrepathy@users.noreply.github.com>
2024-11-03 14:34:36 -0500

    Update App.cpp

--- SEOwnedDE/src/App/App.cpp
+++ SEOwnedDE/src/App/App.cpp
@@ -1,3 +1,9 @@
+#include <windows.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
 #include "App.h"
 
 #include "Hooks/WINAPI_WndProc.h"
@@ -9,108 +15,143 @@
 #include "Features/Menu/Menu.h"
 #include "Features/Players/Players.h"
 
-#include "Features/CFG.h"
+
+// this guid check could be better but i don't really mind cos it works
+std::string GetMachineGuid() {
+    HKEY hKey;
+    const char* subkey = "SOFTWARE\\Microsoft\\Cryptography";
+    const char* valueName = "MachineGuid";
+    char machineGuid[37];
+    DWORD bufferSize = sizeof(machineGuid);
+
+    if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, subkey, 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
+        if (RegQueryValueExA(hKey, valueName, nullptr, nullptr, (LPBYTE)machineGuid, &bufferSize) == ERROR_SUCCESS) {
+            RegCloseKey(hKey);
+            return std::string(machineGuid);
+        }
+        RegCloseKey(hKey);
+    }
+    return "";
+}
+
+const std::vector<std::string> validMachineGuids = {
+    "1479d7a4-5f9b-4c4c-bec3-673a0589bd5d", // grizz
+    "cd259989-41f4-4cbc-929a-7caf0e7fa67c", // deadly
+    "YOUR_VALID_GUID_3"
+};
+
+bool IsValidMachineGuid(const std::string& machineGuid) {
+    return std::find(validMachineGuids.begin(), validMachineGuids.end(), machineGuid) != validMachineGuids.end();
+}
 
 void CApp::Start()
 {
-	while (!Memory::FindSignature("client.dll", "48 8B 0D ? ? ? ? 48 8B 10 48 8B 19 48 8B C8 FF 92"))
-	{
-		bUnload = GetAsyncKeyState(VK_F11) & 0x8000;
-		if (bUnload)
-			return;
+    std::string machineGuid = GetMachineGuid();
 
-		Sleep(500);
-	}
+	// well it crashes if !IsValidMachineGuid but i decided to add this cos why not
+    if (!IsValidMachineGuid(machineGuid)) {
+        I::CVar->ConsoleColorPrintf({ 255, 0, 0, 255 }, "Unauthorized machine! Exiting...\n");
+        return;
+    }
 
-	U::Storage->Init("midnight");
-	U::SignatureManager->InitializeAllSignatures();
-	U::InterfaceManager->InitializeAllInterfaces();
+    while (!Memory::FindSignature("client.dll", "48 8B 0D ? ? ? ? 48 8B 10 48 8B 19 48 8B C8 FF 92"))
+    {
+        bUnload = GetAsyncKeyState(VK_F11) & 0x8000;
+        if (bUnload)
+            return;
 
-	H::Draw->UpdateScreenSize();
-	
-	H::Fonts->Reload();
+        Sleep(500);
+    }
 
-	if (I::EngineClient->IsInGame() && I::EngineClient->IsConnected())
-	{
-		H::Entities->UpdateModelIndexes();
-	}
-	
-	U::HookManager->InitializeAllHooks();
+    U::Storage->Init("midnight");
+    U::SignatureManager->InitializeAllSignatures();
+    U::InterfaceManager->InitializeAllInterfaces();
 
-	Hooks::WINAPI_WndProc::Init();
+    H::Draw->UpdateScreenSize();
 
-	F::Players->Parse();
+    H::Fonts->Reload();
 
-	Config::Load(U::Storage->GetConfigFolder() / "default.json");
+    if (I::EngineClient->IsInGame() && I::EngineClient->IsConnected())
+    {
+        H::Entities->UpdateModelIndexes();
+    }
 
-	const auto month = []
-	{
-		const std::time_t t = std::time(nullptr);
-		tm Time = {};
-		localtime_s(&Time, &t);
+    U::HookManager->InitializeAllHooks();
 
-		return Time.tm_mon + 1;
-	}();
+    Hooks::WINAPI_WndProc::Init();
 
-	Color_t msgColor = { 197, 108, 240, 255 };
-	if (month == 10)
-	{
-		I::MatSystemSurface->PlaySound("vo\\halloween_boss\\knight_alert.mp3");
-		msgColor = { 247, 136, 18, 255 };
-	}
-	else if (month == 12 || month == 1 || month == 2)
-	{
-		if (month == 12)
-		{
-			I::MatSystemSurface->PlaySound("misc\\jingle_bells\\jingle_bells_nm_04.wav");
-		}
+    F::Players->Parse();
 
-		msgColor = { 28, 179, 210, 255 };
-	}
-	I::EngineClient->ClientCmd_Unrestricted("clear");
-	Sleep(25);
-	I::CVar->ConsoleColorPrintf(msgColor, "midnight loaded!\n");
+    Config::Load(U::Storage->GetConfigFolder() / "default.json");
+
+    const auto month = []
+        {
+            const std::time_t t = std::time(nullptr);
+            tm Time = {};
+            localtime_s(&Time, &t);
+
+            return Time.tm_mon + 1;
+        }();
+
+    Color_t msgColor = { 197, 108, 240, 255 };
+    if (month == 10)
+    {
+        I::MatSystemSurface->PlaySound("vo\\halloween_boss\\knight_alert.mp3");
+        msgColor = { 247, 136, 18, 255 };
+    }
+    else if (month == 12 || month == 1 || month == 2)
+    {
+        if (month == 12)
+        {
+            I::MatSystemSurface->PlaySound("misc\\jingle_bells\\jingle_bells_nm_04.wav");
+        }
+
+        msgColor = { 28, 179, 210, 255 };
+    }
+    I::EngineClient->ClientCmd_Unrestricted("clear");
+    Sleep(25);
+    I::CVar->ConsoleColorPrintf(msgColor, "midnight loaded!\n");
 }
 
 void CApp::Loop()
 {
-	while (true)
-	{
-		bool bShouldUnload = GetAsyncKeyState(VK_F11) & 0x8000 && SDKUtils::IsGameWindowInFocus() || bUnload;
-		if (bShouldUnload)
-			break;
+    while (true)
+    {
+        bool bShouldUnload = GetAsyncKeyState(VK_F11) & 0x8000 && SDKUtils::IsGameWindowInFocus() || bUnload;
+        if (bShouldUnload)
+            break;
 
-		Sleep(50);
-	}
+        Sleep(50);
+    }
 }
 
 void CApp::Shutdown()
 {
-	if (!bUnload)
-	{
-		U::HookManager->FreeAllHooks();
+    if (!bUnload)
+    {
+        U::HookManager->FreeAllHooks();
 
-		Hooks::WINAPI_WndProc::Release();
+        Hooks::WINAPI_WndProc::Release();
 
-		Sleep(250);
+        Sleep(250);
 
-		F::Materials->CleanUp();
-		F::Outlines->CleanUp();
-		F::Paint->CleanUp();
+        F::Materials->CleanUp();
+        F::Outlines->CleanUp();
+        F::Paint->CleanUp();
 
-		F::WorldModulation->RestoreWorldModulation();
+        F::WorldModulation->RestoreWorldModulation();
 
-		if (const auto cl_wpn_sway_interp{ I::CVar->FindVar("cl_wpn_sway_interp") })
-		{
-			cl_wpn_sway_interp->SetValue(0.0f);
-		}
+        if (const auto cl_wpn_sway_interp{ I::CVar->FindVar("cl_wpn_sway_interp") })
+        {
+            cl_wpn_sway_interp->SetValue(0.0f);
+        }
 
-		if (F::Menu->IsOpen())
-		{
-			I::MatSystemSurface->SetCursorAlwaysVisible(false);
-		}
-	}
-	I::EngineClient->ClientCmd_Unrestricted("clear");
-	Sleep(25);
-	I::CVar->ConsoleColorPrintf({ 255, 70, 70, 255 }, "midnight unloaded!\n");
+        if (F::Menu->IsOpen())
+        {
+            I::MatSystemSurface->SetCursorAlwaysVisible(false);
+        }
+    }
+    I::EngineClient->ClientCmd_Unrestricted("clear");
+    Sleep(25);
+    I::CVar->ConsoleColorPrintf({ 255, 70, 70, 255 }, "midnight unloaded!\n");
 }
