eacaef0bc645dfbe539a08c2643b934ea3b36cf6
inrepathy <137946035+inrepathy@users.noreply.github.com>
2024-12-07 09:12:22 -0500

    w
    w

--- SEOwnedDE/src/Utils/Singleton/Singleton.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define MAKE_SINGLETON(type, name) namespace MAKE_SINGLETON_INSTANCES { inline type name = {}; } inline type *name = &MAKE_SINGLETON_INSTANCES::name;
-#define MAKE_SINGLETON_SCOPED(type, name, scope) namespace MAKE_SINGLETON_INSTANCES { inline type name = {}; } \
-namespace scope { inline type *name = &MAKE_SINGLETON_INSTANCES::name; }
